d43a6fd6f4c10a4c36cc29e5f161351b8736b042
alex-dll <alexsandrojr.303@gmail.com>
2022-03-20 23:01:37 -0300

    create a template using static data

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,44 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container, MovieList, Movie } from './styles';
 
 function Home() {
+
+  const movies = [
+    {
+      id: 1,
+      title: 'spider-man',
+      image_url: 'https://img.elo7.com.br/product/zoom/3E882CA/big-poster-filme-homem-aranha-sem-volta-para-casa-90x60-cm-1-poster-geek.jpg'
+    },
+    {
+      id: 2,
+      title: 'spider-man',
+      image_url: 'https://img.elo7.com.br/product/zoom/3E882CA/big-poster-filme-homem-aranha-sem-volta-para-casa-90x60-cm-1-poster-geek.jpg'
+    },
+    {
+      id: 3,
+      title: 'spider-man',
+      image_url: 'https://img.elo7.com.br/product/zoom/3E882CA/big-poster-filme-homem-aranha-sem-volta-para-casa-90x60-cm-1-poster-geek.jpg'
+    }
+  ]
+
   return (
-    <h1>Ola Marilene</h1>
+    <Container>
+      <h1>Movies</h1>
+
+      <MovieList>
+        {movies.map(movie => {
+          return (
+            <Movie key={movie.id}>
+              <a href="/">
+                <img src={movie.image_url} alt={movie.title} />
+              </a>
+              <span>{movie.title}</span>
+            </Movie>
+          )
+        })}
+      </MovieList>
+    </Container>
   );
 }
 
